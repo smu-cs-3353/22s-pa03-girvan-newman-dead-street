66e128a5fa4054fedcd74dd6e3e85e53d3cfa35e
agenato <agenato@smu.edu>
2022-03-23 12:14:52 -0500

    Added CMakeLists.txt, gitignore, and src directory

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/***************************************
+* Team: Dead Street
+* Partners: Ashley Genato, Bryce Shurts, & Michael Amberg
+* CS 3353 - PA 03
+***************************************/
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+
+    //type here to begin
+    std::cout << "We can do this!" << std::endl;
+
+    return EXIT_SUCCESS;
+}
