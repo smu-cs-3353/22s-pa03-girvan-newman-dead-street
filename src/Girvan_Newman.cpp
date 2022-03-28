a88f188e43e2856b4a44686e4a8f7d4b6eec6b39
agenato <agenato@smu.edu>
2022-03-28 12:22:06 -0500

    Created Girvan_Newman class

---
+++ src/Girvan_Newman.cpp
@@ -0,0 +1,6 @@
+/******************************************************
+* CS 3353 - PA 03
+* Description: Implementation of the Girvan Newman algorithm
+*******************************************************/
+
+#include "Girvan_Newman.h"
---
+++ src/Girvan_Newman.h
@@ -0,0 +1,19 @@
+/******************************************************
+* CS 3353 - PA 03
+* Description: Header for the Girvan Newman algorithm
+*******************************************************/
+
+#ifndef INC_22S_PA03_GIRVAN_NEWMAN_H
+#define INC_22S_PA03_GIRVAN_NEWMAN_H
+
+#include <boost/graph/adjacency_list.hpp>
+
+class Girvan_Newman {
+    private:
+        boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> test;
+
+    public:
+};
+
+
+#endif //INC_22S_PA03_GIRVAN_NEWMAN_H
