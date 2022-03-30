d20067522c5e0966790448a26fffd9a90f90cef9
Michael Amberg <michaeltamberg@gmail.com>
2022-03-30 14:15:12 -0500

    "Added the Python conversion module, and updated it's path to read/write data"

--- pythonThings/convert.py
+++ pythonThings/convert.py
@@ -4,8 +4,7 @@
 import networkx as nx
 import lxml.etree as lxmletree
 
-path = "/Users/Michael/Desktop/Random SMU docs/football/"
+path = "data/"
 graph = nx.read_gml(path+"football.gml")
-# graphml = nx.generate_graphml(gml)
 nx.write_graphml(graph, path+"converted.graphml")
 
