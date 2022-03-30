529a1447b7ba4a2554c81efd46849d576d872644
agenato <agenato@smu.edu>
2022-03-29 19:07:52 -0500

    Fixed some comments

--- src/Girvan_Newman.h
+++ src/Girvan_Newman.h
@@ -7,21 +7,30 @@
 #define INC_22S_PA03_GIRVAN_NEWMAN_H
 
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/properties.hpp>
+#include <string>
 
 class Girvan_Newman {
     private:
-            boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> test;
-            //^this seems to be class rather than an actual container
+
+            typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,
+                                          boost::property<boost::vertex_name_t, std::string>> Graph;
+            Graph test;
 
             /* for clarification on the parameters in <>:
              * 1st boost::vecS = vertices are stored in a vector
              * 2nd boost::vecS = edges are stored in a vector
              * boost::undirectedS = means the graph is undirected
-             * the last 2 (not listed) are vertex & edge properties, create a struct above for vertex data?
+             * boost::property<boost::vertex_name_t, std::string> = vertices have a string variable
+             *                                                      that represents their name
+             *
+             * Note: Can add two more parameters to define edges' and graph's properties.
+             * We can also redefine the vertices if the GraphML defines them differently
              * */
     public:
-            Girvan_Newman(); //do we need a default constructor?
+            Girvan_Newman(std::string&); //Creates an unweighted and undirected graph from a given GraphML file
             //constructor that will take in a dataset and create the graph
+            //consider using [using namespace boost;] to minimize the boost calls
 };
 
 
