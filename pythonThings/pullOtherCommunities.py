aad3f7d17b6be430329f14b536142a4dd83950c6
Michael Amberg <michaeltamberg@gmail.com>
2022-04-01 12:44:10 -0500

    "Southern women doesnt work for whatever reason but the others do?"

--- pythonThings/pullOtherCommunities.py
+++ pythonThings/pullOtherCommunities.py
@@ -5,8 +5,6 @@ import networkx as nx
 
 G1 = nx.karate_club_graph()
 
-G2 = nx.davis_southern_women_graph()
-
 G3 = nx.florentine_families_graph()
 
 G4 = nx.les_miserables_graph()
@@ -14,7 +12,6 @@ G4 = nx.les_miserables_graph()
 path = "data/"
 
 nx.write_graphml(G1, path+"karateClub.graphml")
-nx.write_graphml(G2, path+"southernWomen.graphml")
 nx.write_graphml(G3, path+"florentineFams.graphml")
 nx.write_graphml(G4, path+"lesMis.graphml")
 
