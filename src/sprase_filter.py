573a4cc89d5caea067f88dbcfb03fe2a562d85ef
S-Bryce <bshurts@smu.edu>
2022-04-11 20:49:03 -0500

    Add backpropagation gradients to sparse filtering

--- src/sprase_filter.py
+++ src/sprase_filter.py
@@ -4,10 +4,30 @@
 * set of data provided by an unweighted, undirected
 * graph.
 ****************************************************"""
+import math
+
 import numpy as np
-import scipy
 from numpy.linalg import norm
 from scipy.optimize import fmin_l_bfgs_b
+
+
+def _back_prop(raw_data, norm_data, norm_sum, delta):
+    return delta / norm_sum - norm_data * (delta * raw_data).sum(axis=1) / (norm_sum ** 2)
+
+
+def _get_attributes(weights, dataset):
+    pre_processed = np.dot(weights, dataset)  # F
+    processed = np.sqrt(pre_processed ** 2 + 1e-8)  # Fs
+
+    # Step 2: L2 normalization (LSE) along features (rows)
+    row_norm_sum = np.linalg.norm(processed, axis=0)
+    row_normalized_matrix = processed / row_norm_sum  # F tilde
+
+    # Step 3: L2 normalization (LSE) along the samples (columns)
+    col_norm_sum = np.linalg.norm(row_normalized_matrix, axis=1)
+    column_normalized_matrix = np.transpose(row_normalized_matrix) / col_norm_sum  # F hat
+
+    return pre_processed, processed, row_norm_sum, row_normalized_matrix, col_norm_sum, column_normalized_matrix
 
 
 def fit_sparse_filter(matrix, features, runs, verb):
@@ -18,19 +38,29 @@ def fit_sparse_filter(matrix, features, runs, verb):
     def _objective_function(weights):
         # Step 1: non-linear processing of dataset with randomized weight matrix weights
         weights = weights.reshape(features, dataset.shape[0])
-        pre_processed = np.dot(weights, dataset)
-        processed = np.sqrt(pre_processed ** 2 + 1e-8)  # F
 
-        # Step 2: L2 normalization (LSE) along features (rows)
-        row_normalized_matrix = processed / norm(processed, axis=0, keepdims=True)  # F tilde
-
-        # Step 3: L2 normalization (LSE) along the samples (columns)
-        column_normalized_matrix = row_normalized_matrix / norm(row_normalized_matrix, axis=1, keepdims=True)  # F hat
+        pre_processed, processed, row_norm_sum, row_normalized_matrix, col_norm_sum, column_normalized_matrix = \
+            _get_attributes(weights, dataset)
 
         # Step 4: Minimize (L-BFGS) the L1 normalization
-        objective_value = norm(column_normalized_matrix, ord=1, keepdims=True)
-        return objective_value
+        objective_value = column_normalized_matrix.sum()
+        new_weights = _back_prop(row_normalized_matrix,
+                                 column_normalized_matrix,
+                                 col_norm_sum,
+                                 np.ones_like(column_normalized_matrix)
+                                 )
 
-    final_weights, f, d = fmin_l_bfgs_b(_objective_function, rand_weights.flatten(), maxfun=runs, iprint=verb,
-                                        approx_grad=True)
-    return final_weights.reshape(features, dataset.shape[0])
+        new_weights = _back_prop(processed,
+                                 np.transpose(row_normalized_matrix),
+                                 row_norm_sum,
+                                 np.transpose(new_weights)
+                                 )
+
+        new_weights = (new_weights * (pre_processed / processed)).dot(np.transpose(dataset))
+
+        return objective_value, new_weights.flatten()
+
+    final_weights, f, d = fmin_l_bfgs_b(_objective_function, rand_weights.flatten(), maxfun=runs, iprint=verb)
+    final_weights = final_weights.reshape(features, dataset.shape[0])
+
+    return _get_attributes(final_weights, dataset)[5]  # column_normalized_matrix
